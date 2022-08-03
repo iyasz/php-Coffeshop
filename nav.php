44918d0f4335b280a1a5978fcb06aaf7b5398c69
Yasz Avellia <zakamaragames@gmail.com>
2022-08-03 20:18:32 +0700

    nav v3

--- nav.php
+++ nav.php
@@ -12,16 +12,23 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-dark shadow-sm p-3">
+<nav class="navbar navbar-expand-lg bg-dark shadow-sm p-2">
   <div class="container-fluid">
-    <a class="navbar-brand text-light mb-0 h1 ms-5 fs-4" href="#">CoffeShop</a>
+<nav class="navbar ">
+  <div class="container-fluid me-5 ms-3">
+    <a class="navbar-brand text-light h1 mb-0 fs-4 "  href="#">
+      <img src="logo.png" width="38" height="34" class="d-inline-block align-text-top ">
+      CoffeShop
+    </a>
+  </div>
+</nav>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
         <li class="nav-item">
-          <a class="nav-link active text-light ms-5" aria-current="page" href="#">Home</a>
+          <a class="nav-link active text-light ms-2" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link text-light ms-2" href="#">Menu</a>
