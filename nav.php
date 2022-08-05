84a15dfffee4ed974487835962986fb6a2792cb6
Yasz Avellia <zakamaragames@gmail.com>
2022-08-05 16:42:56 +0700

    mengubah nav v4

--- nav.php
+++ nav.php
@@ -22,10 +22,10 @@
     </a>
   </div>
 </nav>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler text-light " type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon font-light"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
+    <div class="collapse navbar-collapse " id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
         <li class="nav-item">
           <a class="nav-link active text-light ms-2" aria-current="page" href="#">Home</a>
